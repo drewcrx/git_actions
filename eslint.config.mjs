1b1470e3bbbf7db5c1f082d66d27673f14110244
Andrew <abv.carrera@yavirac.edu.ec>
2026-05-30 15:05:57 -0500

    initial ci cd setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"]
+    }
+  }
+];
